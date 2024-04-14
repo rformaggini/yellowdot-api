5f8fb607cbab001c5c8aa8ec9f39bce58b5715a7
Rodney Formaggini <rformaggini@gmail.com>
2024-04-14 08:28:54 -0300

    first commit

---
+++ src/main/java/com/yellowdot/yellowdotapi/YellowdotApiApplication.java
@@ -0,0 +1,13 @@
+package com.yellowdot.yellowdotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowdotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellowdotApiApplication.class, args);
+	}
+
+}
