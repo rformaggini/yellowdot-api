e3ec07867a0b8c6fbb24999f6003ce8a43f16df0
Rodney Formaggini <rformaggini@gmail.com>
2024-04-18 16:35:17 -0300

    change name repository

--- src/main/java/com/yellowdot/yellowdotapi/repositories/RoleRepository.java
+++ src/main/java/com/yellowdot/yellowdotapi/repositories/RoleRepository.java
@@ -4,8 +4,6 @@ import com.yellowdot.yellowdotapi.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
