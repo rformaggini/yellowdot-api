72ceb0fd5fa37eb63860d5c9b412adab4fd4999c
Rodney Formaggini <rformaggini@gmail.com>
2024-05-01 17:23:15 -0300

    Refactoring RestMapping

--- src/main/java/com/yellowdot/yellowdotapi/controllers/UserController.java
+++ src/main/java/com/yellowdot/yellowdotapi/controllers/UserController.java
@@ -1,25 +1,16 @@
-package com.yellowdot.yellowdotapi.controller;
+package com.yellowdot.yellowdotapi.controllers;
 
 import com.yellowdot.yellowdotapi.dtos.CreateUserDto;
-import com.yellowdot.yellowdotapi.entities.Role;
 import com.yellowdot.yellowdotapi.entities.User;
-import com.yellowdot.yellowdotapi.repositories.RoleRepository;
-import com.yellowdot.yellowdotapi.repositories.UserRepository;
 import com.yellowdot.yellowdotapi.services.UserService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Set;
 
-@RestController("/api/v1/users")
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService userService;
@@ -28,13 +19,13 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<Void> createUser(@RequestBody CreateUserDto userDto){
         userService.createUser(userDto);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping()
+    @GetMapping
     @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public ResponseEntity<List<User>> listUsers(){
         return ResponseEntity.ok(userService.getAllUsers());
