2c18160eabf946bf98546ec6b93221b640927d8d
Rodney Formaggini <rformaggini@gmail.com>
2024-09-17 13:34:50 -0300

    Updated cors policy

--- src/main/java/com/yellowdot/yellowdotapi/config/SecurityConfig.java
+++ src/main/java/com/yellowdot/yellowdotapi/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
